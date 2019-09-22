b0242ed4860fac7473b55ea6f1d60839c8d49234
jakechvatal <jakechvatal@github.com>
2019-09-22 01:12:02 -0400

    gah

--- src/initialState.js
+++ src/initialState.js
@@ -1,4 +1,19 @@
 export default initialState = {
-    "yes": "no"
-    
+    "localState": {
+        "loggedIn": false,
+        "guardian": false,
+        "curLat": 0,
+        "curLong": 0
+    },
+    "cars": {
+        "locations":[]
+    },
+    "disaster": {
+        "radius": 0,
+        "lat": 0,
+        "long": 0
+    },
+    "stats": {
+        
+    }
 }
--- src/reducers/locationsReducer.js
+++ src/reducers/locationsReducer.js
@@ -1,9 +1,22 @@
 import initialState from '../initialState';
+import axios from 'axios';
 
 const LocationsR = (state = initialState, action) => {
     switch(action.type) {
-        case 'LOGIN_SUCCESS':
-            return;
+        case 'GET_ASSIGNMENT':
+            axios.get('https://hestia-bigredhacks2019.herokuapp.com/api/getmyassignment')
+            .then = (response => {
+                state.stats = {
+                    lat: response.data.latitude,
+                    long: response.data.longitude,
+                    numRiders: response.data.numRiders,
+                    maxNumRiders: response.data.maxNumRiders,
+                    pending: response.data.pending
+                }}).catch(error => {
+                    console.log(error);
+                });
+
+        
     }
 
     return initialState;
